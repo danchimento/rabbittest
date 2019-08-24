0ca1171da241689fa5943ce4b71be315a278b161
Dan Chimento <danchimento@live.com>
2019-08-23 23:34:08 -0400

    working sample

---
+++ Orchestrator/Orchestrator.Communication/ICommunicator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Orchestrator.Communication
+{
+    public interface ICommunicator
+    {
+        Task Send<T>(T request);
+    }
+}
---
+++ aspnetapp/aspnetapp/Controllers/SendController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetapp.Models;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace aspnetapp.Controllers
+{
+    [Route("api/send")]
+    public class SendController : Controller
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            Send();
+            return "Sent";
+        }
+
+        private void Send()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "cardunload",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "cardunload",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
